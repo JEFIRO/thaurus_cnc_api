c11f79fb8bf0119941e8823c570560b5dd46d90d
Jefferson Vitena <150871603+JEFIRO@users.noreply.github.com>
2025-11-01 13:32:18 -0300

    Add `StatusInvalidoException` handler and fix status code for `DadosInvalidosException`

--- src/main/java/com/jefiro/thaurus_cnc/infra/exception/RestExceptionHendler.java
+++ src/main/java/com/jefiro/thaurus_cnc/infra/exception/RestExceptionHendler.java
@@ -16,7 +16,12 @@ public class RestExceptionHendler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(DadosInvalidosException.class)
     private ResponseEntity<HandlerException> handleException(DadosInvalidosException exception) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new HandlerException(HttpStatus.NOT_FOUND, exception.getMessage()));
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new HandlerException(HttpStatus.BAD_REQUEST, exception.getMessage()));
+    }
+
+    @ExceptionHandler(StatusInvalidoException.class)
+    private ResponseEntity<HandlerException> handleException(StatusInvalidoException exception) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new HandlerException(HttpStatus.BAD_REQUEST, exception.getMessage()));
     }
 
 
